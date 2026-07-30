3bc274c8de942032542284172f3b90a6322920a5
Pranav1065 <maxpranav2009@gmail.com>
2026-07-30 10:15:08 +0530

    Create Exercise Q2.c

---
+++ DSU/Practical 2/Practical 2C/Exercise Q2.c
@@ -0,0 +1,18 @@
+//Write a C Program to Check if a Number is Divisible by 2.
+Program
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if((num & 1) == 0)
+        printf("Number is divisible by 2");
+    else
+        printf("Number is not divisible by 2");
+
+    return 0;
+}
