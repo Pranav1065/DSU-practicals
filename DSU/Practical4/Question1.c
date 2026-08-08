058f04638fb86ac407666f23397ca8e66317d846
Pranav1065 <maxpranav2009@gmail.com>
2026-08-09 00:12:29 +0530

    Create Question1.c

---
+++ DSU/Practical4/Question1.c
@@ -0,0 +1,42 @@
+//Write a C Program to Implement Recursive Binary Search.
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    if(low > high)
+        return -1;
+
+    int mid = (low + high) / 2;
+
+    if(arr[mid] == key)
+        return mid;
+
+    if(key < arr[mid])
+        return binarySearch(arr, low, mid - 1, key);
+
+    return binarySearch(arr, mid + 1, high, key);
+}
+
+int main()
+{
+    int arr[100], n, i, key, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted array elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, 0, n - 1, key);
+
+    if(result != -1)
+        printf("Element found at position %d", result + 1);
+    else
+        printf("Element not found");
+
+    return 0;
+}
