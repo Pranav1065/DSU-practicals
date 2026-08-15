760aec1ab249b7be4fe790c7a3f63bea269f4c3e
Pranav1065 <maxpranav2009@gmail.com>
2026-08-15 21:55:05 +0530

    Create Question5.c

---
+++ DSU/Practical 5/5B/Question5.c
@@ -0,0 +1,46 @@
+//Count Number of Comparisons Performed
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[10][20], temp[20];
+    int n, i, j;
+    int comparisons = 0;
+
+    printf("Enter number of strings: ");
+    scanf("%d", &n);
+
+    printf("Enter strings:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%s", str[i]);
+    }
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - 1 - i; j++)
+        {
+            comparisons++;
+
+            if(strcmp(str[j], str[j + 1]) > 0)
+            {
+                strcpy(temp, str[j]);
+                strcpy(str[j], str[j + 1]);
+                strcpy(str[j + 1], temp);
+            }
+        }
+    }
+
+    printf("Sorted strings:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%s\n", str[i]);
+    }
+
+    printf("Total comparisons = %d", comparisons);
+
+    return 0;
+}
