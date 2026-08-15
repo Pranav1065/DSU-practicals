5083737d8b0052adb41e5329050401237875427f
Pranav1065 <maxpranav2009@gmail.com>
2026-08-15 21:15:24 +0530

    Create Question 4.c

---
+++ DSU/Practical 3/3A q.c/Question 4.c
@@ -0,0 +1,31 @@
+//Find the smallest element using Linear Search.
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    min = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    printf("Smallest element = %d", min);
+
+    return 0;
+}
