256a6bbf0839b46247a7d2bb337fecd28f71b4dd
Pranav1065 <maxpranav2009@gmail.com>
2026-08-15 21:14:50 +0530

    Create Question 3.c

---
+++ DSU/Practical 3/3A q.c/Question 3.c
@@ -0,0 +1,31 @@
+//Find the largest element using Linear Search.
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, max;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
